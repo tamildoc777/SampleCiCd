64c6215ab8e934e1f46325619e92fd1a5c3346be
tamildoc777 <tamildoc777@gmail.com>
2023-01-25 16:51:08 +0530

    MainActivity.kt update

--- app/src/main/java/com/example/samplecicd/MainActivity.kt
+++ app/src/main/java/com/example/samplecicd/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.samplecicd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("test7","777")
     }
 }
